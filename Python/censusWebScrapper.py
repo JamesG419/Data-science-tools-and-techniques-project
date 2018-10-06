0f7e004d91ec3317a00df52e9a646c054d5feb4f
James Glenister <jglen419@gmail.com>
2018-10-06 07:28:06 -0400

    added workflow comments. added code that extracts weblinks

--- Python/censusWebScrapper.py
+++ Python/censusWebScrapper.py
@@ -1,10 +1,20 @@
 import requests
 from bs4 import BeautifulSoup, SoupStrainer
 
-webPage = requests.get("https://www.census.gov/programs-surveys/popest.html")
+
+#extract web links
+webLink = "https://www.census.gov/programs-surveys/popest.html"
+webPage = requests.get(webLink)
 
 rawHtml = webPage.text
 
 soup = BeautifulSoup(rawHtml, 'html.parser')
 
-print(soup.prettify())
+for link in soup.find_all('a'):
+    print(link.get('href'))
+
+#cast to absolute URIs
+
+#verifies no duplicates
+
+#save to CSV file
